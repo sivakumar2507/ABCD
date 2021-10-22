bf0a1fc3f815ea06c8f88b3d20e5e5c0dbf5be3c
sivakumar2507 <sivakumarvelmuruganvms@gmail.com>
2021-10-22 11:40:22 +0530

    ABCD

---
+++ src/org/Loop.java
@@ -0,0 +1,16 @@
+package org;
+
+public class Loop {
+	public static void main(String[] args) {
+		for (int i = 1; i <=7; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.println(i);
+				
+				System.out.println();
+				
+			}
+			
+		}
+	}
+
+}
